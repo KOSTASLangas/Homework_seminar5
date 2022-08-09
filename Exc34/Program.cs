251666559c77907e3173ca8f866d3c170a95a9ff
Kostas <langas.kostya@mail.ru>
2022-08-09 14:11:42 +0500

    Нахождение и подсчет четных чисел массива

--- Exc34/Program.cs
+++ Exc34/Program.cs
@@ -3,11 +3,11 @@
 Console.WriteLine("Введите кол-во элементов массива");
 int n = Convert.ToInt32(Console.ReadLine());
 int[] array = new int[n];
-for ( int i = 0; i < 5; i++)
+for ( int i = 0; i < n; i++)
     array[i] = new Random().Next (100, 1000);
 Console.WriteLine( "[" + string.Join("," , array)+ "]");
 int count = 0;
-for ( int i = 0; i < 5; i++)
+for ( int i = 0; i < n; i++)
 if (array[i] % 2 == 0)
 count++;
 
