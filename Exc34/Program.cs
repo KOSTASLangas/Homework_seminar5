687d215a0901737acffd169a90b9cd2d4bf97ee2
Kostas <langas.kostya@mail.ru>
2022-08-09 14:06:31 +0500

    Нахождение и подсчет четных чисел массива

---
+++ Exc34/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите кол-во элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for ( int i = 0; i < 5; i++)
+    array[i] = new Random().Next (100, 1000);
+Console.WriteLine( "[" + string.Join("," , array)+ "]");
+int count = 0;
+for ( int i = 0; i < 5; i++)
+if (array[i] % 2 == 0)
+count++;
+
+Console.WriteLine("В массиве " + count +  " четных чисел" );
