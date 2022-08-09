312fcb846eeb1ec178c1e6882986397fa419fb40
Kostas <langas.kostya@mail.ru>
2022-08-09 14:31:25 +0500

    Нахождение суммы чисел на нечетных позициях

---
+++ Exc36/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите кол-во элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for ( int i = 0; i < n; i++)
+    array[i] = new Random().Next (0, 101);
+Console.WriteLine( "[" + string.Join("," , array)+ "]");
+
+int count = 0;
+for ( int i = 1; i < n; i++)
+{
+    if (i % 2 != 0)
+    count += array[i];    
+}
+Console.WriteLine(count);
